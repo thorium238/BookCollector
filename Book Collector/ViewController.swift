616bae7c65ad414a5d9949d2504d8c7f7d00e4fc
Thomas Piotrowski <Thomas.Piotrowski@gmail.com>
2016-08-29 10:52:12 -0500

    Initial Commit

---
+++ Book Collector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Book Collector
+//
+//  Created by Thomas Piotrowski on 8/29/16.
+//  Copyright © 2016 Thomas Piotrowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
